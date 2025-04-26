41c7645297ac60b0669f5871274671ff9cb09fa0
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-04-26 12:17:47 +1000

    fix: Исправлено название сервиса

--- configs/__init__.py
+++ configs/__init__.py
@@ -2,11 +2,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class ProjectConfiguration(BaseSettings):
-    model_config = SettingsConfigDict(env_prefix="MANAGER_")
+    model_config = SettingsConfigDict(env_prefix="PREPROCESSING_")
 
     # * Опциональные переменные
     DEBUG_MODE: bool = True
-    SERVICE_NAME: str = "ilps-service-task-manager"
+    SERVICE_NAME: str = "ilps-service-audio-preprocessing"
 
 
 configs = ProjectConfiguration()
