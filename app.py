63f55c58c6207475b23e64ea23e834e7a7fffb14
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-05-20 22:59:31 +1000

    feature: Пакет сервисного логгера (#1)

--- app.py
+++ app.py
@@ -1,20 +1,35 @@
+import hashlib
 from contextlib import asynccontextmanager
+from random import randbytes
+from typing import Callable
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 from routers import health_router, preprocessing_router
+from service_logging import logger
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # on_startup
+    # До запуска приложения
+    logger.info("FastAPI application starting up...")
 
     yield
 
-    # on_shutdown
+    # После запуска
+    logger.info("FastAPI application shutting down...")
 
 
 service = FastAPI(lifespan=lifespan)
+
+
+@service.middleware("http")
+async def add_request_hash(request: Request, call_next: Callable):
+    request_hash = hashlib.sha1(randbytes(32)).hexdigest()[:10]
+    with logger.contextualize(request_hash=request_hash):
+        response = await call_next(request)
+        return response
+
 
 service.include_router(health_router)
 service.include_router(preprocessing_router)
