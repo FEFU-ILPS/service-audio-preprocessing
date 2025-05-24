40d794c3ed65922c62a904363fa7c79f67edc0a8
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-05-24 20:18:34 +1000

    feature: Конфигурация graylog

--- configs/__init__.py
+++ configs/__init__.py
@@ -1,8 +1,13 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+from .graylog import GraylogConfiguration
+
 
 class ProjectConfiguration(BaseSettings):
     model_config = SettingsConfigDict(env_prefix="PREPROCESSING_")
+
+    # * Вложенные группы настроек
+    graylog: GraylogConfiguration = GraylogConfiguration()
 
     # * Опциональные переменные
     DEBUG_MODE: bool = True
